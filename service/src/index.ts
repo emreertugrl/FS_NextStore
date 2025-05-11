e559f75f2fcc9abf71b4d1184a4172cb95a6f4b0
emreertugrl <emrertugrl@gmail.com>
2025-05-11 13:43:25 +0300

    add mongoose integration

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import mongoose from "mongoose";
+
 // express çağırılır.
 const app = express();
 // port belirlenir.
@@ -7,6 +9,11 @@ const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
 });
+
+mongoose
+  .connect("mongodb://localhost:27017/nextStoredb")
+  .then(() => console.log("Veritabanına bağlanıldı ✔✔✔"))
+  .catch((err) => console.log("Veritabanına bağlanamadı 💣💣💣", err));
 
 // portun dinlenmesi
 app.listen(port, () => {
