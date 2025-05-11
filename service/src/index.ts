30f2e0b7f8b6fbc475e4b9992bdf4e8f7c0babc2
emreertugrl <emrertugrl@gmail.com>
2025-05-11 13:46:45 +0300

    add dotenv integration

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,17 +1,20 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 // express çağırılır.
 const app = express();
+// middlewares
+dotenv.config();
 // port belirlenir.
-const port = process.env.PORT || 3000;
+const port = (process.env.PORT as string) || 3000;
 // gelen isteklere cevap verme
 app.get("/", (req, res) => {
   res.send("Hello World!!!");
 });
 
 mongoose
-  .connect("mongodb://localhost:27017/nextStoredb")
+  .connect(process.env.DATABASE_URL as string)
   .then(() => console.log("Veritabanına bağlanıldı ✔✔✔"))
   .catch((err) => console.log("Veritabanına bağlanamadı 💣💣💣", err));
 
