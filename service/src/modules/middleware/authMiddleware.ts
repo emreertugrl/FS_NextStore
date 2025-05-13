ad70422852610c0d4c11e5cb8b9d847ce394e13a
emreertugrl <emrertugrl@gmail.com>
2025-05-13 20:02:47 +0300

    auth middleware fixed

--- service/src/modules/middleware/authMiddleware.ts
+++ service/src/modules/middleware/authMiddleware.ts
@@ -2,12 +2,17 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
 function authMiddleware(req: Request, res: Response, next: NextFunction) {
-  const token = req.cookies.refreshToken;
-  if (!token) return res.status(401).json({ message: "Unauthorized" });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized - No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_REFRESH_SECRET as string);
-    req.user = decoded;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+    req.user = decoded; // decoded'da user id, role vb. olur
     next();
   } catch (err) {
     return res.status(401).json({ message: "Invalid token" });
